10f60d74a71c069d94553560efa38abca2edc511
ThanhPH <phamhathanh130394@gmail.com>
2017-03-29 18:27:07 +0700

    Multiple windows.

--- main.js
+++ main.js
@@ -3,11 +3,45 @@
 const { app, ipcMain, dialog, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
-let window;
+const dataPath = //path.join(app.getPath(documents), '/sticky_notes/');
+    app.getAppPath();
 
-function createWindow() {
-    window = new BrowserWindow({
+let mainWindow;
+
+function initialize() {
+    mainWindow = new BrowserWindow({
+        show: true,
+        width: 0,
+        height: 0,
+        frame: false,
+        transparent: true
+    });
+
+    const DATA_FILE_NAME = 'data.json';
+    fs.readFile(DATA_FILE_NAME, (error, data) => {
+        let notes;
+        if (error) {
+            notes = [{ text: '' }];
+            fs.writeFile(DATA_FILE_NAME, JSON.stringify(notes), function (error) {
+                if (error)
+                    console.log(error);
+            });
+        }
+        else
+            notes = JSON.parse(data);
+
+        notes.forEach(note => {
+            createWindow(note.text);
+        });
+        // TODO: Create a map with the objects.
+    });
+}
+
+function createWindow(text) {
+    // TODO: Position.
+    const window = new BrowserWindow({
         show: false,
         width: 200,
         height: 200,
@@ -16,7 +50,9 @@ function createWindow() {
         minimizable: false,
         maximizable: false,
         closable: false,
-        hasShadow: true
+        hasShadow: true,
+        skipTaskbar: true,
+        parent: mainWindow
     });
     window.once('ready-to-show', () => {
         window.show();
@@ -27,8 +63,11 @@ function createWindow() {
         slashes: true
     }));
     window.webContents.on('did-finish-load', () => {
-        window.webContents.send('content', 'content goes here');
+        window.webContents.send('content', text);
     });
+
+    // TODO: Close one = close all.
+    // Save content when close.
 }
 
-app.on('ready', createWindow);
+app.on('ready', initialize);
